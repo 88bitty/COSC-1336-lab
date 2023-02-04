ff6c2a305fc3db04dbe110fd06084e0cf2a01821
8bitty <max@koeninger.org>
2023-02-04 15:51:56 -0600

    lab completed

--- Koeninger_Lab4_2.py
+++ Koeninger_Lab4_2.py
@@ -1,2 +1,29 @@
 #My name is Max Koeninger
-#This program is incomplete
+#This program is complete
+
+budget = 0
+expenseTotal = 0
+
+while True:
+    budget = float(input('What is your monthly budget? '))
+    if(budget > 0):
+        break
+    else:
+        print('Your budget must be above 0')
+
+while True:
+    expense = float(input('Please enter an expense(0 to stop): '))
+    expenseTotal += expense
+    if(expense == 0):
+        break
+    else:
+        pass
+
+print(f'Your monthly budget is: ${budget:,.2f}')
+print(f'Your monthly expenses are: ${expenseTotal:,.2f}')
+
+budget -= expenseTotal
+if(budget > 0):
+    print(f'Your remaining money is: ${budget:,.2f}')
+else:
+    print(f'You are ${abs(budget):,.2f} over budget')
