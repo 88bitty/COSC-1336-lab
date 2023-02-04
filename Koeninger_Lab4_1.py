83064ede54f5b1459baec82c158a5d3ab3a235dd
8bitty <max@koeninger.org>
2023-02-04 14:56:02 -0600

    Lab 4.1 complete

--- Koeninger_Lab4_1.py
+++ Koeninger_Lab4_1.py
@@ -1,2 +1,12 @@
 #My name is Max Koeninger
 #This program is incomplete
+
+yrs_total = 0
+sea_total = 0
+
+print("Year\tSea rise (millimeters)")
+print("------------------------------")
+for i in range(30):
+    yrs_total +=1
+    sea_total +=1.2
+    print(f"{yrs_total}\t{sea_total:.2f}")
