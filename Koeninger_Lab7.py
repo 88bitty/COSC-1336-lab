cdd6c2edb17810517be7270c88f90a98106ee0a5
8bitty <max@koeninger.org>
2023-03-12 21:53:17 -0500

    lab 7 now compliant with input validation rules

--- Koeninger_Lab7.py
+++ Koeninger_Lab7.py
@@ -2,41 +2,55 @@
 #This program is complete
 
 #This function appends user input to a list a number of times the user specifies.
-#It checks for ValueErrors and other errors in both user inputs.
+#provided the input is 1 or greater. It also accepts input for a number to be used for value tests.
+#It checks for ValueErrors and other errors, and validates that all inputs are numbers and 1 or above.
 def main():
     nums = []
+    valid = False
     try:
-        size = int(input("Enter the number of elements in the list: "))
-    except ValueError:
-        print("Incorrect data type. Program terminated.")
-        return
-    except:
-            print("Unexpected error. Program terminated.")
-            return
-    for num in range(size):
-        try:
-            tempNum = int(input("Enter a number: "))
+        while valid == False:
+            try:
+                size = round(float(input("Enter the number of elements in the list: ")))
+                if(size>=1 and type(size)==int):
+                    valid = True
+                else:
+                    print("The number must be 1 or more.")
+            except ValueError:
+                print("Your input must be a number")
+        for num in range(size):
+            valid = False
+            while valid == False:
+                try:
+                    tempNum = round(float(input("Enter a number: ")))
+                    if(type(tempNum) == int):
+                        valid = True
+                    else:
+                        print("Your input must be a number")
+                except ValueError:
+                    print("Your input must be a number")
             nums.append(tempNum)
-        except ValueError:
-            print("Incorrect data type: a number was expected. Program terminated.")
-            return
-        except:
-            print("Unexpected error. Program terminated.")
-            return
-    display_larger(nums)
-
-#This function takes a list and user input for a number, and creates new list with all numbers
-#from the original list that are larger than the input number. It checks for valueErrors and other errors.
-def display_larger(numList):
-    newList = []
-    try:
-        testNum = int(input("Enter the number you wish to test if the list elements are greater than: "))
+        valid = False
+        while valid == False:
+            try:
+                testNumber = round(float(input("Enter the number you wish to test if the list elements are greater than: ")))
+                if(type(testNumber) == int):
+                    valid = True
+                else:
+                    print("Your input must be a number")
+            except ValueError:
+                print("Your input must be a number")
     except ValueError:
         print("Incorrect data type. Program terminated.")
         return
     except:
         print("Unexpected error. Program terminated.")
         return
+    display_larger(nums, testNumber)
+
+#This function accepts a list and a testing number, and appends all numbers in the list
+#to a new list, provided they are above the testing number.
+def display_larger(numList,testNum):
+    newList = []
     for num in numList:
         if num > testNum:
             newList.append(num)
